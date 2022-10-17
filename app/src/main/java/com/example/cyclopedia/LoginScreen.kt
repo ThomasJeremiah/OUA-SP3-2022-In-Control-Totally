32bcf19cf54d2d336ab10c05cd1f4eaf010d10ec
ThomasJeremiah <107043807+ThomasJeremiah@users.noreply.github.com>
2022-10-17 14:01:19 +1100

    Refining UI elements

--- app/src/main/java/com/example/cyclopedia/LoginScreen.kt
+++ app/src/main/java/com/example/cyclopedia/LoginScreen.kt
@@ -1,13 +1,15 @@
 package com.example.cyclopedia
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login_screen.*
 
-
+var loginCredential:String = ""
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,10 @@ class LoginScreen : AppCompatActivity() {
         supportActionBar?.hide()
 
         setContentView(R.layout.activity_login_screen)
+
+        editTextEmail.visibility = View.INVISIBLE
+        editTextTextPassword.visibility = View.INVISIBLE
+
 
         homeBtn.setOnClickListener{
             startActivity(Intent(this,MainActivity::class.java))
@@ -29,7 +35,37 @@ class LoginScreen : AppCompatActivity() {
         textViewForgotPassword.setOnClickListener{
             /*Start account recovery process. */
         }
+        emailLoginBtn.setOnClickListener{
+            editTextEmail.visibility = View.VISIBLE
+            emailLoginBtn.visibility = View.INVISIBLE
+            passwordLoginBtn.visibility = View.INVISIBLE
+        }
+        passwordLoginBtn.setOnClickListener{
+            editTextTextPassword.visibility = View.VISIBLE
+            emailLoginBtn.visibility = View.INVISIBLE
+            passwordLoginBtn.visibility = View.INVISIBLE
+        }
+        submitBtn.setOnClickListener{
+            if(editTextEmail.visibility == View.VISIBLE){
+                loginCredential = editTextEmail.text.toString()
+                hideKeyboard()
+                Toast.makeText(this@LoginScreen,"$loginCredential",Toast.LENGTH_SHORT).show()
+            } else {
+                if (editTextTextPassword.visibility == View.VISIBLE){
+                    loginCredential = editTextTextPassword.text.toString()
+                    hideKeyboard()
+                    Toast.makeText(this@LoginScreen,"$loginCredential",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@LoginScreen,"Please select a login option",Toast.LENGTH_SHORT).show()
+                }
+            }
+            editTextEmail.visibility = View.INVISIBLE
+            editTextTextPassword.visibility = View.INVISIBLE
+            emailLoginBtn.visibility = View.VISIBLE
+            passwordLoginBtn.visibility = View.VISIBLE
+        }
 
+        /**Button behaviour changed to independent. Toggle logic left for reference.
         /*Listens for changes in the toggle button group.
         This is independent of the Google and Facebook login options,
         might need to add some logic to have only a single selection between the 4 options, not just the two. */
@@ -48,7 +84,13 @@ class LoginScreen : AppCompatActivity() {
                     //None selected - enforce selection here...
                 }
             }
-        }
+        }*/
     }
-
+    fun Activity.hideKeyboard(view:View){
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    }
+    fun Activity.hideKeyboard(){
+        hideKeyboard(currentFocus?:View(this))
+    }
 }
